3abdb726f0f121b3ec206e0de1fb8ebede79e3d1
jmcolella <colella.john@gmail.com>
2016-07-28 15:52:22 -0400

    create models, migrations and corresponding spec files for votes and comments

---
+++ db/migrate/20160728195046_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :body
+      t.references :user
+      t.references :article
+      t.references :voteable, polymorphic: true, index: true
+
+      t.timestamps
+    end
+  end
+end
