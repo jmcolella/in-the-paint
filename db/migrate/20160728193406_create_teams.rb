f3c999dd1236c76fa8f02d8600fe7b21b2b9bc8a
Ayaz Uddin <ayaz2589@gmail.com>
2016-07-30 18:16:40 -0400

    Seed API not done

--- db/migrate/20160728193406_create_teams.rb
+++ db/migrate/20160728193406_create_teams.rb
@@ -2,8 +2,8 @@ class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
 
-    	t.string :name, null: false
-    	t.integer :api_id, null: false
+    	t.string :name
+    	t.integer :team_id
 
       t.timestamps null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'nba_api_wrapper'
+
+NBA::Stats::Team.get_teams.each { |team| Team.create(name: team[1], team_id: team[0]) }
+
+def get_player(param) 
+	NBA::Player.get_player(param)
+end
+
+Team.all.each do |t|
+	NBA::Player.get_all_players['resultSets'][0]['rowSet'][0].each do |p|
+		Player.create(
+			name: get_player((p[0])['resultSets'][1]['rowSet'][0][1]), 
+			position: get_player(p[0])['resultSets'][0]['rowSet'][0][14], 
+			team_id: get_player(p[0])['resultSets'][0]['rowSet'][0][16], 
+			number: get_player(p[0])['resultSets'][0]['rowSet'][0][13], 
+			height: get_player(p[0])['resultSets'][0]['rowSet'][0][10], 
+			weight: get_player(p[0])['resultSets'][0]['rowSet'][0][11], 
+			average_points: get_player(p[0])['resultSets'][1]['rowSet'][0][3], 
+			average_assists: get_player(p[0])['resultSets'][1]['rowSet'][0][4], 
+			average_rebounds: get_player(p[0])['resultSets'][1]['rowSet'][0][5]
+			) unless t.team_id != p[7]
+	end
+end
+
