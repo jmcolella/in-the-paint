188c977523b9d36e8089c86cceea411501230439
jmcolella <colella.john@gmail.com>
2016-08-02 15:12:29 -0400

    add index and show actions to Articles controller and create one spec test for index; add routes for Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,13 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+
+  def show
+    @article = Article.find_by(params[:id])
+    @comments = @article.comments
+  end
+
 end
