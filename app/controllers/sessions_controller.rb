b723b114c7d00ad50da599b80c138874b086e011
ICalvinG <calvinieong1118@gmail.com>
2016-08-02 17:02:27 -0400

    Add Sessions new, create, destroy routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,31 @@
 class SessionsController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.new(user_params)
+    	if user.save
+    	  session[:user_id] = user.id
+    	end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+    	if @user
+      	   @user.destroy
+    	else
+      	   render '404'
+    	end
+  	end
+
+  	private
+
+  	def user_params
+    	params.require(:user).permit(:username, :email, :password)
+  	end
+
+end
+
 end
